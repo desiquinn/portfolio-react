397c89f152238db21c57fc971eeb61ca08560f9e
Desiree Morris <desiquinncompany@gmail.com>
2019-09-02 16:30:20 -0400

    formatted the new project and contact forms

---
+++ src/components/Forms/contact.js
@@ -0,0 +1,100 @@
+import React, {useState} from 'react';
+
+const NewProjectForm = () => {
+    const[message, setMessage] = useState({
+        name: "",
+        email: "",
+        website: "",
+        description: "",
+    })
+
+    const handleChange= (event) => {
+        console.log({[event.target.name]:event.target.value})
+        setMessage({
+            ...message,
+            [event.target.name]:event.target.value
+        })
+    }
+
+
+    return (
+        <div>
+            <div>
+                <h3>Employers</h3>
+                <p>I am currently looking for full time employment.
+                    If you'd like to interview me, please contact me in the following ways:
+                    Phone Number: (704)819-2526
+                    Email: desiquinncompany@gmail.com
+                </p>
+            </div>
+
+            <div>
+                <h3> Connect With Me on Social Media </h3>
+                <img src="#" atl="GitHub"/>
+                <img src="#" atl="LinkedIN"/>
+                <img src="#" atl="Instagram"/>
+                <img src="#" atl="Facebook"/>
+            </div>
+
+            <h3>General Inquiries</h3>
+
+            <div>
+                <h4>Where to Email?</h4>
+                <p>Hire Me Full Time:</p>
+                <p>Collaborate With Me:</p>
+                <p>General Questions:</p>
+                <p>Speaking Engagements:</p>
+
+                <h4>Other Ways to Contact Me</h4>
+                <img src="#" atl="phone number" />
+                <p>Phone Number: 704-819-2526</p>
+                <img src="#" atl="address" />
+                <p>Mail: 
+                    Desiree Morris
+                    4652 Perth Ct
+                    Apt. 6 
+                    Charlotte NC 28215</p>
+            </div>
+            <div>
+                <h4>Send Me a Message</h4>
+                <p>Use this form for general inquiries.  If you want to start a new project with me please visit
+                    the New Project Form to tell me more about your vision.  
+                </p>
+        
+                <form onSubmit={handleSubmit}>
+                    <label>Name</label>
+                    <input 
+                        type='text'
+                        name='name'
+                        value={message.name}
+                        onChange= {handleChange}
+                    />
+                    <label>Email Address</label>
+                    <input 
+                        type='email'
+                        name='email'
+                        value={message.email}
+                        onChange= {handleChange}
+                    />
+                    <label>Website URL</label>
+                    <input 
+                        type='text'
+                        name='website'
+                        value={message.website}
+                        onChange= {handleChange}
+                    />
+                    <label>How Can I Help You?</label>
+                    <input 
+                        type='text'
+                        name='description'
+                        value={message.description}
+                        onChange= {handleChange}
+                    />
+                    <button>Submit</button>
+                </form>
+            </div>
+        </div>
+    )
+};
+
+export default NewProjectForm;
